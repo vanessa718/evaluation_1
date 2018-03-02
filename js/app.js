fe49a2de450605150f80c8afa00b1f7ad2047cf3
vanessa <vanessa.chaillou@orange.fr>
2018-03-02 16:48:59 +0100

    latest commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+
+console.log('hello');
+(function main() { 
+	var full="";
+
+/* for add the class expenses*/
+	$('#btn1').click(function(){
+		var exp=0;
+
+			$(".a").each(function(){
+				exp+=($(this).val())*1;
+			console.log(exp);
+			});
+			$("#add").val(exp);	
+
+/* for add class saving */
+			$(".b").each(function(){
+				exp+=($(this).val())*1;
+			});
+			$("#add").val(exp);
+		});
+
+
+	
+
+
+	if('.o'===full){
+		return false;
+		alert('Veuillez remplir le champ');
+	}
+})();
+
+
+
+
+
---
+++ js/app1.js
@@ -0,0 +1,98 @@
+(function main(){
+
+
+	var resultat = 0;
+console.log(resultat);
+
+	$('input').blur(function()  {
+		var expenses = 'rent_expenses'+'credit_expenses'+'energetic_expenses'+'phone_expenses'+'home_insurance'+'car_insurance'+'mutual_health'+'care_expenses'+'icome_taxes'+'local_taxes'+'grocery'+'gasoline'+'sport_culture'+'hobbies'
+
+		var a = 
+
+
+		});
+	
+
+		// Calculate();
+		// return false;
+		// console.log("click?");
+			// $(".calculation").html(result);
+	function Calculate(){
+		
+
+
+		// function expenses(){
+	//variables for add expenses	
+	// 	   var my1 = parseInt(document.getElementById('rent_expenses').value);
+	// console.log(document.getElementById('rent_expenses'));
+	// 	   var my2 = parseInt(document.getElementById('credit_expenses').value);
+		   // var my3 = document.getElementById('energetic_expenses').value;
+		   // var my4 = document.getElementById('phone_expenses').value;
+		   // var my5 = document.getElementById('home_insurance').value;
+		   // var my6 = document.getElementById('car_insurance').value;
+		   // var my7 = document.getElementById('mutual_health').value;
+		   // var my8 = document.getElementById('care_expenses').value;
+		   // var my9 = document.getElementById('icome_taxes').value;
+		   // var my10 = document.getElementById('local_taxes').value;
+		   // var my11 = document.getElementById('grocery').value;
+		   // var my12 = document.getElementById('gasoline').value;
+		   // var my13 = document.getElementById('sport_culture').value;
+		   // var my14 = document.getElementById('hobbies').value;
+		// var resultat = my1 + my2;
+		// alert(resultat);
+		//     my1 = ( isNaN( my1 ) && (my1 != '' )) ? my1 : 0 ;
+		//     my2 = ( isNaN( my2 ) && (my2 != '' )) ? my2 : 0 ;
+		//     my3 = ( isNaN( my3 ) && (my3 != '' )) ? my3 : 0 ;
+		//     my4 = ( isNaN( my4 ) && (my4 != '' )) ? my4 : 0 ;	     
+		//     my5 = ( isNaN( my5 ) && (my5 != '' )) ? my5 : 0 ;	     
+		//     my6 = ( isNaN( my6 ) && (my6 != '' )) ? my6 : 0 ;	     
+		//     my7 = ( isNaN( my7 ) && (my7 != '' )) ? my7 : 0 ;	     
+		//     my8 = ( isNaN( my8 ) && (my8 != '' )) ? my8 : 0 ;
+		//     my9 = ( isNaN( my9 ) && (my9 != '' )) ? my9 : 0 ;	     
+		//     my10 = ( isNaN( my10 ) && (my10 != '' )) ? my10 : 0 ;
+		//     my11 = ( isNaN( my10 ) && (my10 != '' )) ? my11 : 0 ;
+		//     my12 = ( isNaN( my10 ) && (my10 != '' )) ? my12 : 0 ;
+		//     my13 = ( isNaN( my10 ) && (my10 != '' )) ? my13 : 0 ;
+		//     my14 = ( isNaN( my10 ) && (my10 != '' )) ? my14 : 0 ;
+		// document.getElementById('result').value = parseFloat(my1) + parseFloat(my2) + parseFloat(my3) + parseFloat(my4) + parsefloat(my5) + parseFloat(my6) + parseFloat(my7) + parseFloat(my8) + parseFloat(my9) + parseFloat(my10) + parseFloat(my11) + parseFloat(my12) + parseFloat(my13) + parseFloat(my14);
+		// }
+
+
+		// function revenue(){   
+		// //variables for add revenue/saving
+		// 	var r1 = document.getElementById('wages').value;
+		// 	var r2 = document.getElementById('aids').value;
+		// 	var r3 = document.getElementById('annuities').value;
+		// 	var r4 = document.getElementById('other').value;
+		// 	var r5 = document.getElementById('saving').value;
+
+		//     r1 = ( isNaN( r1 ) && (r1 != '' )) ? r1 : 0 ;
+		//     r2 = ( isNaN( r2 ) && (r2 != '' )) ? r2 : 0 ;
+		//     r3 = ( isNaN( r3 ) && (r3 != '' )) ? r3 : 0 ;
+		//     r4 = ( isNaN( r4 ) && (r4 != '' )) ? r4 : 0 ;
+		//     r5 = ( isNaN( r5 ) && (r5 != '' )) ? r5 : 0 ;
+		// document.getElementById('result').value =  parseFloat(r1) + parseFloat(r2) + parseFloat(r3) + parseFloat(r4) + parsefloat(r5);
+		// }
+
+
+		// // function addition(){
+		// // 	return parseFloat(my1) + parseFloat(my2) + parseFloat(my3) + parseFloat(my4) + parsefloat(my5) + parseFloat(my6) + parseFloat(my7) + parseFloat(my8) + parseFloat(my9) + parseFloat(my10) + parseFloat(my11) + parseFloat(my12) + parseFloat(my13) + parseFloat(my14);
+		// // 	}    
+
+		// function add(){
+		// 	return parseFloat(r1) + parseFloat(r2) + parseFloat(r3) + parseFloat(r4) + parsefloat(r5);
+		// }	
+
+		// }
+	}
+
+
+
+				// value = $(this).html();
+				// $(".calculation").append(value);
+	 
+})();
+
+	// function add(){
+	// 	if ('.form-group') {}
+	// }
